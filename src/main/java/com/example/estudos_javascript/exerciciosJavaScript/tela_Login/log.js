c7725b85bb1e3a2b0553bb35bfbccd4f0e65b55a
Henrique Strada <henriquestradax@gmail.com>
2023-09-08 22:31:09 -0300

    Commit

--- src/main/java/com/example/estudos_javascript/exerciciosJavaScript/tela_Login/log.js
+++ src/main/java/com/example/estudos_javascript/exerciciosJavaScript/tela_Login/log.js
@@ -1,10 +1,14 @@
 let usuario = prompt("Digite o nome de usuário:");
 let senha = prompt("Digite a senha:");
 
-let desejaLogar = confirm("Você deseja fazer login?");
 
+
+let valorName = prompt("Digite o nome de usuario");
+let valorSenha = prompt("Digite a senha:");
+
+let desejaLogar = confirm("Você deseja fazer login?");
 if (desejaLogar) {
-    if (usuario === "nomeDeUsuario" && senha === "senhaCorreta") {
+    if (usuario === valorName && senha === valorSenha) {
         alert("Logado com sucesso!");
     } else {
         alert("Falha ao logar.");
